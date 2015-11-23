6b8f307bf14c57ee636a04ac8f3a46d8728c02cc
CodeDotJS <rushforlinux@gmail.com>
2015-11-24 01:06:53 +0530

    added image converter :heart:

---
+++ JavaScript/js/index.js
@@ -0,0 +1,39 @@
+var reader = new FileReader();
+var img = document.createElement('img');
+var inp = document.getElementById('inp');
+
+var loadFile = function(event) {
+  var file = event.target.files[0];
+  inp.style.display = 'none';
+  showImage(file);
+};
+
+var showImage = function(file) {
+  img.src = URL.createObjectURL(file);
+  document.body.appendChild(img);
+  document.body.style['background-image'] = 'none';
+};
+
+var color = [
+  '#ff0000',
+  '#ffffff',
+  '#0000ff'
+];
+
+var createRainbow = function() {
+  var can = document.createElement('canvas'),
+    ctx = can.getContext('2d'),
+    w = img.width,
+    h = img.height,
+    l = Math.ceil(h / 3),
+    i;
+  can.width = w;
+  can.height = h;
+  for (i = 0; i < 3; i++) {
+    ctx.fillStyle = color[i];
+    ctx.fillRect(0, i * l, w, l);
+  }
+};
+
+img.addEventListener('load', createRainbow);
+inp.addEventListener('change', loadFile);
